efd09977913619d0b457633b09afb56a2f02a34b
TKB <TKB@TIMOTHYs-MacBook-Pro.local>
2017-06-24 09:07:37 -0400

    complete

--- app.js
+++ app.js
@@ -14,7 +14,24 @@ app.get('/directory', function(req, res){
   res.render('directory', data);
 });
 
+app.get('/directory/:id', function(req, res){
+ const filterList = data.users
+  var user;
 
+  for(var i = 0; i < data.users.length; i++) {
+    if(data.users[i].id == req.params.id) {
+     user = data.users[i];
+    }
+  }
+//   var skill;
+// // for(var i=0; i<user.skills.length; i++) {
+// //   skill = user.skills[i];
+// //   console.log(skill);
+// // }
+  // console.log('user', user);
+
+  res.render('profile', user);
+});
 
 
 
